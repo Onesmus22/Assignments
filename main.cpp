cfde765e5b2bd0c66097f78bd348c14b560c5b4d
ARYAMANYA ONESMUS <onesmusaryamanya2@gmail.com>
2025-03-19 09:48:56 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,59 +1,64 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+#include <iostream>
 
 using namespace std;
 #define MAX 5
 class stack{
+private:
+  int top;
+  string friends[MAX];
 
 public: stack(){
    top=-1;
    }
-   void push(int val){
+   void push(string friendName){
      if(top == MAX-1){
         cout<<"overflow"<<endl;
      } else{
       top +=1;
-      arr[top]=val;
+      friends[top]=friendName;
      }
    }
    int pop(){
+    string friendName;
      if(top==-1){
         cout<<"underflow"<<endl;
      }else{
         top=top-1;
-         return arr[top];
+        friends[top]=friendName;
      }
 
    }
-    int peek(){
+    /*int peek(){
       if(top==-1){
         cout<<"stack is empty"<<endl;
       }else{
-       cout<<"my peek is:"<<arr[top]<<"\n\n";
+       cout<<"my peek is:"<<friends[top]<<"\n\n";
       }
-    }
+    }*/
 
 void printstack(){
      for(int i=0; i<=top; i++){
-        cout<<arr[i]<<endl;
+        cout<<friends[i]<<endl;
      }
    }
-   private:
-  int top;
-  int arr[MAX];
+
 };
 int main()
 {
     stack s;
-    s.push(45);
-    s.push(50);
-    s.push(55);
-    s.push(60);
-    s.push(65);
+    s.push("onesmus");
+    s.push("ronald");
+    s.push("phil");
+    s.push("zaq");
+    s.push("vicent");
     s.pop();
-    s.peek();
+    //s.peek();
     s.printstack();
 
 
     return 0;
 }
-
